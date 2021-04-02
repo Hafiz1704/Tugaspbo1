d2d6ae96cf087e541d44312a1a7a97056fabfa78
hafiz1704 <hafizansyari856@gmail.com>
2021-04-02 11:29:06 +0700

    membuat inputoutput

---
+++ InputOutput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ public static void main(String[]args){
+  String nama;
+  int usia;
+  double ipk;
+
+  Scanner scanner = new Scanner(System.in);
+
+  System.out.print("input nama : ");
+  nama = scanner.nextLine();
+
+  System.out.print("input usia : ");
+  usia = scanner.nextInt();
+
+  System.out.print("input ipk : ");
+  ipk = scanner.nextDouble();
+
+  System.out.println("Hai " + nama);
+  System.out.println("usia " + usia);
+  System.out.println("ipk " + ipk);
+ }
+}
---
+++ model/Mahasiswa.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilkanInfo(){
+  System.out.println("Nama : Hafiz");
+  System.out.println("IPK : 3.99");
+ }
+}
